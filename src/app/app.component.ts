13a88b70e6387139fae183db89f2a64b43a5e473
Robbe Croes <robbe.croes@student.vives.be>
2021-01-20 17:08:44 +0100

    fourth  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'examen-angular';
 
-  item_colors = ["Geel", "Groen", "Blauw", "Rood", "Paars"];
-  item_images = ["assets/geel.jpg", "assets/groen.jpg", "assets/blauw.png","assets/rood.jpg", "assets/paars.jpg"];
+  item_colors = ["Geel", "Groen", "Blauw", "Rood", "Paars"]; // Hier kunnen we een nieuw kleur toevoegen.
+  item_images = ["assets/geel.jpg", "assets/groen.jpg", "assets/blauw.png","assets/rood.jpg", "assets/paars.jpg"]; // hier kunnen we een nieuwe afbeelding toevoegen.
+// de afbeelding en de naam van het kleur moeten in de juiste volgorde staan,
+// anders zal een verkeerd kleur worden getoond.
 
   selected_item=0;
 
